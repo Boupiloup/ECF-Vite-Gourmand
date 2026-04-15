6e28a5d16386579dc4bd6e6ac80160726e2f666e
Boupiloup <nxmodss@gmail.com>
2026-04-15 22:48:05 +0200

    ajout du fichier inscription finis

---
+++ public/inscription.php
@@ -0,0 +1,81 @@
+<?php
+session_start();
+$pageTitle = "Inscription";
+require_once '../includes/db.php';
+include_once __DIR__ . '/../includes/header.php';
+
+$message = null;
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $nom = trim($_POST['nom'] ?? '');
+    $prenom = trim($_POST['prenom'] ?? '');
+    $email = trim($_POST['email'] ?? '');
+    $password = $_POST['password'] ?? '';
+
+
+    if (empty($nom) || empty($prenom) || empty($email) || empty($password)) {
+        $message = "Tous les champs sont obligatoires.";
+    } elseif (strlen($password) < 8) {
+        $message = "Le mot de passe doit contenir au moins 8 caractères.";
+    }
+     else {
+        $sql = "SELECT * FROM utilisateur WHERE email = ?";
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute([$email]);
+        $user = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        if ($user) {
+            $message = "Un compte avec cet email existe déjà. Veuillez choisir un autre email.";
+        } else {
+            $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+            $sql = "INSERT INTO utilisateur (nom, prenom, email, mot_de_passe, role_id) VALUES (?, ?, ?, ?, ?)";
+            $stmt = $pdo->prepare($sql);
+            $stmt->execute([$nom, $prenom, $email, $hashedPassword, 2]);
+
+            header('Location: connexion.php');
+            exit;
+        }
+    }
+}
+?>
+
+<form method="POST">
+    <div class="form-container">
+
+        <h2>Inscription</h2>
+
+        <div class="form-inscription">
+            <label for="nom">Nom:</label>
+            <input type="text" id="nom" name="nom" placeholder="Nom" required>
+        </div>
+        <div class="form-inscription">
+            <label for="prenom">Prénom:</label>
+            <input type="text" id="prenom" name="prenom" placeholder="Prénom" required>
+        </div>
+        <div class="form-inscription">
+            <label for="email">Email:</label>
+            <input type="email" id="email" name="email" placeholder="Email" required>
+        </div>
+        <div class="form-inscription">
+            <label for="password">Mot de passe:</label>
+            <input type="password" id="password" name="password" placeholder="Mot de passe" required>
+            <p class="form-help">Minimum 8 caractères</p>
+        </div>
+
+
+        <?php if ($message): ?>
+            <p class="error-message"><?= htmlspecialchars($message) ?></p>
+        <?php endif; ?>
+
+        <button type="submit">S'inscrire</button>
+
+        <div class="form-footer">
+            <p>Vous avez déjà un compte ?</p>
+            <a href="connexion.php" class="link-register">Se connecter</a>
+        </div>
+    </div>
+</form>
+
+<?php
+include_once __DIR__ . '/../includes/footer.php';
+?>
